095f24c8cabfda321fd57c1c6f66d3b449dd333b
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-02-13 16:36:32 +0530

    Update webpage_access.py

--- webpage_access.py
+++ webpage_access.py
@@ -4,26 +4,4 @@ import streamlit.components.v1 as components
 st.title("webpage accessed")
 
 # components.iframe("https://py-slack.virajkhatavkar.com", width=800, height=600)
-# CSS कोड जोड़ें
-st.markdown(
-    """
-    <style>
-    iframe {
-        width: 100%;
-        height: 100%;
-        position: fixed;
-        top: 0;
-        left: 0;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-components.html(
-    """
-    <iframe src="https://www.portfolioyoga.com/wp/"></iframe>
-    """,
-    height=0,
-    width=0
-)
+ components.iframe("https://www.portfolioyoga.com/wp/", width=1000, height=1000)
