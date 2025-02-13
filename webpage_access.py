f267722d2678b41b60a7e4e5a53648127a9f74ac
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-02-13 16:35:22 +0530

    Update webpage_access.py

--- webpage_access.py
+++ webpage_access.py
@@ -9,8 +9,8 @@ st.markdown(
     """
     <style>
     iframe {
-        width: 100vw;
-        height: 100vh;
+        width: 100%;
+        height: 100%;
         position: fixed;
         top: 0;
         left: 0;
