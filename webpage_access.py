56347b2af13b810c386ccdd089a1e8e3b1e1b0f8
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-02-13 16:29:40 +0530

    Update webpage_access.py

--- webpage_access.py
+++ webpage_access.py
@@ -3,4 +3,5 @@ import streamlit.components.v1 as components
 
 st.title("webpage accessed")
 
-components.iframe("https://py-slack.virajkhatavkar.com", width=800, height=600)
+# components.iframe("https://py-slack.virajkhatavkar.com", width=800, height=600)
+components.iframe("https://www.bhaskar.com/epaper", width=800, height=600)
