850eff98cc3631a9774e37fdf5b1d461faa76038
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-02-13 16:52:25 +0530

    Update webpage_access.py

--- webpage_access.py
+++ webpage_access.py
@@ -4,4 +4,4 @@ import streamlit.components.v1 as components
 st.title("webpage accessed")
 
 # components.iframe("https://py-slack.virajkhatavkar.com", width=800, height=600)
-components.iframe("https://www.nseindia.com/market-data/live-equity-market?symbol=NIFTY%20IT", width=1000, height=1000, scrolling=True)
+components.iframe("https://www.nseindia.com/market-data/advance", width=1000, height=1000, scrolling=True)
