e6036eb3942b29a4cf7b784664b9c9d7b317a5bd
prayan2702 <165507774+prayan2702@users.noreply.github.com>
2025-02-13 17:06:12 +0530

    Update webpage_access.py

--- webpage_access.py
+++ webpage_access.py
@@ -4,4 +4,4 @@ import streamlit.components.v1 as components
 st.title("webpage accessed")
 
 # components.iframe("https://py-slack.virajkhatavkar.com", width=800, height=600)
-components.iframe("https://www.niftytrader.in/advance-decline-ratio", width=1000, height=1000, scrolling=True)
+components.iframe("https://www.tickertape.in/market-mood-index", width=1000, height=1000, scrolling=True)
